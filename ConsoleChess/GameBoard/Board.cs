00e72213a087095e1c28a7310883958a17c1f0f6
Stefano Tejo <stefano.tejo@gmail.com>
2020-01-13 00:04:24 -0300

    Implemented BoardException, and board methods to verify if there's a piece in a certain position, as well as validating piece placement

--- ConsoleChess/GameBoard/Board.cs
+++ ConsoleChess/GameBoard/Board.cs
@@ -17,13 +17,44 @@ namespace ConsoleChess.GameBoard
             Pieces = new GamePiece[rows, columns];
         }
 
+        public bool IsValidPosition(Position position)
+        {
+            if(position.Row < 0 || position.Row >= Rows || position.Column < 0 || position.Column > Columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidatePosition(Position position)
+        {
+            if(!IsValidPosition(position))
+            {
+                throw new BoardException("ERROR: Invalid position");
+            }
+        }
+
+        public bool IsThereGamePiece(Position position)
+        {
+            ValidatePosition(position); // Throws exception if position not valid, ending execution of IsThereGamePiece()
+            return GetPiece(position) != null;
+        }
+
         public GamePiece GetPiece(int row, int column)
         {
             return Pieces[row, column];
         }
+        public GamePiece GetPiece(Position position)
+        {
+            return Pieces[position.Row, position.Column];
+        }
 
         public void PlacePiece(GamePiece piece, Position position)
         {
+            if(IsThereGamePiece(position))
+            {
+                throw new BoardException("ERROR: There is already a game piece at this position");
+            }
             Pieces[position.Row, position.Column] = piece;
             piece.ActualPosition = position;
         }
---
+++ ConsoleChess/GameBoard/BoardException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleChess.GameBoard
+{
+    class BoardException : ApplicationException
+    {
+        public BoardException(string message) : base(message)
+        {
+        }
+    }
+}
