629931f84ed1ff10fe3b892804f11e7bf4f27e69
Stefano Tejo <stefano.tejo@gmail.com>
2020-01-14 15:06:11 -0300

    Removed unnecessary using statements

--- ConsoleChess/Chess/ChessMatch.cs
+++ ConsoleChess/Chess/ChessMatch.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using ConsoleChess.GameBoard;
-using ConsoleChess.Chess;
 
 namespace ConsoleChess.Chess
 {
--- ConsoleChess/Chess/Pawn.cs
+++ ConsoleChess/Chess/Pawn.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ConsoleChess.GameBoard;
+﻿using ConsoleChess.GameBoard;
 
 namespace ConsoleChess.Chess
 {
